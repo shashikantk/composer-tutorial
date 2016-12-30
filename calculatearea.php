db12b5e9616530cfa67d5b3b3064a442995c4c9b
Shashikant Kuswaha <shashikant.kuswaha@sts.local>
2016-12-30 16:55:41 +0530

    added

---
+++ calculatearea.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+  $side = isset($argv[1]) ? $argv[1] : null;
+
+  if (isset($side) && ctype_digit($side)) {
+    Shapes\Square::area($side);
+  }
+  else {
+    echo "Please provide a valid size!\n";
+  }
---
+++ source/Square.php
@@ -0,0 +1,10 @@
+<?php
+namespace Shapes;
+
+  class Square {
+    static function area($side){
+      $surface_area = $side * $side;
+
+      echo "The squares surface area is $surface_area units\n";
+    }
+  }
